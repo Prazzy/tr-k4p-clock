e4db6fc97c4c16967eccf269913391b8c378f319
Praveen <prazzee@gmail.com>
2016-03-16 14:35:53 -0700

    custom changes

--- public/pana-theme.js
+++ public/pana-theme.js
@@ -3,7 +3,11 @@
  */
 
 import chrome from 'ui/chrome';
-import kibanaLogoUrl from 'plugins/popular-theme/images/PanasonicLogo.png';
+import kibanaLogoUrl from 'plugins/popular-theme/images/panasky_logo_white.png';
 
 chrome
+.setBrand({
+  'logo': 'url(' + kibanaLogoUrl + ') left no-repeat',
+  'smallLogo': 'url(' + kibanaLogoUrl + ') left no-repeat'
+})
 .setNavBackground('#08519c');
